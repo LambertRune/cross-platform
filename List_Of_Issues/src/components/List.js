57d2c9b94b351319524256dea7219df843e05c67
lambertR <rune.lambert@student.vives.be>
2025-10-15 11:06:53 +0200

    lesson 3

---
+++ List_Of_Issues/src/components/List.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Text } from 'react-native'
+import {  issues } from './issues'
+
+function list() {
+    return({
+        
+    }
+}
+
+const First = () => {    
+    return(
+        issues.map({
+            
+        })
+    )
+}
+
+export default First
